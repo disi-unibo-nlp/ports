2770be17f656f41923dada9a51351b07874e0f86
Nicolò <nicolo.monaldini@studio.unibo.it>
2024-06-01 17:39:42 +0200

    added web interface

--- main/try_llama_index.py
+++ main/try_llama_index.py
@@ -1,3 +1,6 @@
+import os
+import sys
+import io
 import torch
 from llama_index.llms.huggingface import HuggingFaceLLM
 from llama_index.core.agent import ReActAgent
@@ -7,7 +10,6 @@ from llama_index.core import VectorStoreIndex
 from llama_index.core.objects import ObjectIndex
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core import Settings
-import os
 from llama_index.tools.notion import NotionToolSpec
 from llama_index.tools.yahoo_finance import YahooFinanceToolSpec
 from llama_index.tools.wikipedia import WikipediaToolSpec
@@ -15,6 +17,7 @@ from llama_index.tools.wolfram_alpha import WolframAlphaToolSpec
 from llama_index.core.tools.tool_spec.load_and_search import (
     LoadAndSearchToolSpec,
 )
+import gradio as gr
 
 
 hf_token = os.getenv('HF_KEY')
@@ -25,12 +28,12 @@ retr_model_name_or_path = "BAAI/bge-base-en-v1.5"
 infer_model_name_or_path = "/proj/mounted/models--meta-llama--Meta-Llama-3-8B-Instruct/snapshots/c4a54320a52ed5f88b7a2f84496903ea4ff07b45"
 
 # WARNING: 4-BIT ENCODING DONE HERE, CONSIDER CHANGING (code from llamaindex llama 3 cookbook from)
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.float16,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_use_double_quant=True,
-)
+# quantization_config = BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_compute_dtype=torch.float16,
+#     bnb_4bit_quant_type="nf4",
+#     bnb_4bit_use_double_quant=True,
+# )
 tokenizer = AutoTokenizer.from_pretrained(
     infer_model_name_or_path,
     token=hf_token,
@@ -43,13 +46,13 @@ llm = HuggingFaceLLM(
     model_name=infer_model_name_or_path,
     model_kwargs={
         "token": hf_token,
-        # "torch_dtype": torch.bfloat16,  # comment this line and uncomment below to use 4bit
-        "quantization_config": quantization_config
+        "torch_dtype": torch.bfloat16,  # comment this line and uncomment below to use 4bit
+        # "quantization_config": quantization_config
     },
     generate_kwargs={
-        "do_sample": True,
-        "temperature": 0.6,
-        "top_p": 0.9,
+        "do_sample": False,
+        "temperature": 0,
+        "top_p": 0,
     },
     tokenizer_name=infer_model_name_or_path,
     tokenizer_kwargs={"token": hf_token},
@@ -77,17 +80,28 @@ Settings.llm = llm
 # subtract_tool = FunctionTool.from_defaults(fn=subtract)
 
 all_tools = []
-# all_tools.extend(YahooFinanceToolSpec().to_tool_list())
-# all_tools.extend(WolframAlphaToolSpec(app_id=wolfram_token).to_tool_list())
+all_tools.extend(YahooFinanceToolSpec().to_tool_list())
+all_tools.extend(WolframAlphaToolSpec(app_id=wolfram_token).to_tool_list())
 wikipedia_tools = WikipediaToolSpec().to_tool_list()
-all_tools.extend(LoadAndSearchToolSpec.from_defaults(wikipedia_tools[0], index_kwargs={'llm': None}).to_tool_list())
+wikipedia_wrapper = LoadAndSearchToolSpec.from_defaults(wikipedia_tools[0])
+all_tools.extend(wikipedia_wrapper.to_tool_list())
 obj_index = ObjectIndex.from_objects(
     all_tools,
     index_cls=VectorStoreIndex,
 )
-obj_retriever = obj_index.as_retriever(similarity_top_k=10)
+obj_retriever = obj_index.as_retriever(similarity_top_k=2)
 
-query = "who is ben affleck married to?"
+# system_message = (
+#     "You are a model focused on function calling. You will be given a query and some tools to leverage. "
+#     "If you find it useful, you can call the tools to help answer the query. "
+#     "Just make sure to read the tools documentation well and understand the right parameters to pass."
+# )
+
+# prompt_template = (
+#     f'<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{system_message}<|eot_id|>'
+#     f'<|start_header_id|>user<|end_header_id|>\n\n{{}}<|eot_id|>'
+#     '<|start_header_id|>assistant<|end_header_id|>\n\n'
+# )
 # get tools retrieved
 # retrieved_tools_example = obj_retriever.retrieve(query)
 # for i, tool in enumerate(retrieved_tools_example):
@@ -97,5 +111,18 @@ agent = ReActAgent.from_tools(tool_retriever=obj_retriever, llm=llm, verbose=Tru
 # response = agent.chat(query)
 # print(response)
 # for llm querying only: response = llm.complete("How are you?" + tokenizer.eos_token)
+
+def run_query(query):
+    # output = io.StringIO()
+    # old_stdout = sys.stdout
+    # sys.stdout = output
+    response = agent.chat(query)
+    # sys.stdout = old_stdout
+    # verbose_output = output.getvalue()
+    return response
+
+iface = gr.Interface(fn=run_query, inputs="text", outputs="text").launch()
+
+
 
 
